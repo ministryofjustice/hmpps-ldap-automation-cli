01167ab4a642ad72cac3f81f7457c8187bfdbbbc
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-08 21:22:19 +0100

    temp no db

--- cli/ldap_automation/__init__.py
+++ cli/ldap_automation/__init__.py
@@ -1,5 +1,6 @@
 from ldap3 import Server, Connection, ALL
-import oracledb
+
+# import oracledb
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,11 +12,11 @@ def ldap_connect(ldap_host, ldap_user, ldap_password):
     )
 
 
-def db_connect(db_user, db_password, db_host, db_port, db_service_name):
-    return oracledb.connect(db_user, db_password, db_host, db_port, db_service_name)
+# def db_connect(db_user, db_password, db_host, db_port, db_service_name):
+#     return oracledb.connect(db_user, db_password, db_host, db_port, db_service_name)
 
 
-def db_command(db_command, db_connection=None):
-    db_connection = db_connect() if not db_connection else db_connection
-    with db_connection:
-        return db_connection.cursor().execute(db_command)
+# def db_command(db_command, db_connection=None):
+#     db_connection = db_connect() if not db_connection else db_connection
+#     with db_connection:
+#         return db_connection.cursor().execute(db_command)
