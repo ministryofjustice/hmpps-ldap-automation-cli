a25a6ce32d9bac35aea7c289bd2827fc7ec4a674
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-08 17:56:37 +0100

    remove restraint on odb version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="ldap-automation",
     version="0.1",
     py_modules=["cli"],
-    install_requires=["Click", "ldap3", "oracledb==1.2.2"],
+    install_requires=["Click", "ldap3", "oracledb"],
     entry_points="""
         [console_scripts]
         ldap-automation=cli:cli
