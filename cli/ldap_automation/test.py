10e9c94cc1813f4ec5b5949f1a82d47bfde1c4d3
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-08 21:27:53 +0100

    test search whole dir

--- cli/ldap_automation/test.py
+++ cli/ldap_automation/test.py
@@ -7,8 +7,8 @@ import click
 def test_search():
     ldap_connection = ldap_connect(config.ldap_host, config.ldap_user, config.ldap_password)
     ldap_connection.search(
-        "ou=Users,dc=moj,dc=com",
-        "(&(cn=*)(objectClass=person))",
+        "dc=moj,dc=com",
+        "(objectClass=*)",
         search_scope=SUBTREE,
         attributes=["*"],
     )
