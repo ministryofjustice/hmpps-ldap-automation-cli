6a86d330d125e88fca64cec4d90ec4e9ef6f2723
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-08 21:03:00 +0100

    temp remove dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="ldap-automation",
     version="0.1",
     py_modules=["cli"],
-    install_requires=["Click", "ldap3", "oracledb"],
+    # install_requires=["Click", "ldap3", "oracledb"],
+    install_requires=["Click", "ldap3"],
     entry_points="""
         [console_scripts]
         ldap-automation=cli:cli
