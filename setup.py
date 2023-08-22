8dbcf548cd44f3c1939ad8f5819c5ccfc633bdcc
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-22 15:38:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="ldap-automation",
     version="0.1",
     packages=find_packages(),
-    # install_requires=["Click", "ldap3", "oracledb"],
     install_requires=["Click", "ldap3", "PyGithub", "python-dotenv", "pyjwt", "GitPython"],
     entry_points="""
         [console_scripts]
