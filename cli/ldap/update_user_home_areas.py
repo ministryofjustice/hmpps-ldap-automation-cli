11e13f9f25f893f7ccbc2aac81decd26f7ffdade
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-22 15:43:31 +0100

    remove quotes unneeded

--- cli/ldap/update_user_home_areas.py
+++ cli/ldap/update_user_home_areas.py
@@ -7,21 +7,23 @@ from ldap3 import MODIFY_REPLACE
 
 
 def update_user_home_areas(old_home_area, new_home_area, attribute="userHomeArea"):
-        logging.info(f"Updating user home areas from {old_home_area} to {new_home_area}")
-        conn = ldap_connect(config.ldap_host, config.ldap_user, config.ldap_password)
+    logging.info(f"Updating user home areas from {old_home_area} to {new_home_area}")
+    conn = ldap_connect(config.ldap_host, config.ldap_user, config.ldap_password)
 
-        base_dn = "ou=Users,dc=moj,dc=com" # change this
-        search_filter = f"'(&(objectclass=NDUser)(userHomeArea={old_home_area})(!(cn={old_home_area}))(!(endDate=*)))'" # and this
-        conn.search(base_dn, search_filter, attributes=[attribute])
+    base_dn = "ou=Users,dc=moj,dc=com"  # change this
+    search_filter = (
+        f"(&(objectclass=NDUser)(userHomeArea={old_home_area})(!(cn={old_home_area}))(!(endDate=*)))"  # and this
+    )
+    conn.search(base_dn, search_filter, attributes=[attribute])
 
-        # Iterate through the search results and update the attribute
-        for entry in conn.entries:
-            dn = entry.entry_dn
-            changes = {attribute: [(MODIFY_REPLACE, [new_home_area])]}
-            conn.modify(dn, changes)
+    # Iterate through the search results and update the attribute
+    for entry in conn.entries:
+        dn = entry.entry_dn
+        changes = {attribute: [(MODIFY_REPLACE, [new_home_area])]}
+        conn.modify(dn, changes)
 
-            # Check if the modification was successful
-            if conn.result['result'] == 0:
-                print(f'Successfully updated {attribute} for {dn}')
-            else:
-                print(f'Failed to update {attribute} for {dn}: {conn.result}')
+        # Check if the modification was successful
+        if conn.result["result"] == 0:
+            print(f"Successfully updated {attribute} for {dn}")
+        else:
+            print(f"Failed to update {attribute} for {dn}: {conn.result}")
