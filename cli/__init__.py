fd99db146df85b30a00063f8dcc18568d0f558b8
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-22 21:28:58 +0100

    add shorthand options

--- cli/__init__.py
+++ cli/__init__.py
@@ -11,8 +11,8 @@ def main_group():
 
 
 @click.command()
-@click.option("--user-ou", help="OU to add users to, defaults to ou=Users", default="ou=Users")
-@click.option("--root-dn", help="Root DN to add users to", default="dc=moj,dc=com")
+@click.option("-u", "--user-ou", help="OU to add users to, defaults to ou=Users", default="ou=Users")
+@click.option("-r", "--root-dn", help="Root DN to add users to", default="dc=moj,dc=com")
 @click.argument("user-role-list", required=True)
 def add_roles_to_users(user_ou, root_dn, user_role_list):
     cli.ldap.add_roles_to_username.process_user_roles_list(user_role_list, user_ou, root_dn)
@@ -28,7 +28,7 @@ def update_user_home_areas(old_home_area, new_home_area):
 
 
 @click.command()
-@click.option("--rbac-repo-tag", help="RBAC repo tag to use", default="master")
+@click.option("-tag", "--rbac-repo-tag", help="RBAC repo tag to use", default="master")
 def rbac_uplift(rbac_repo_tag):
     cli.ldap.rbac.main(rbac_repo_tag)
 
